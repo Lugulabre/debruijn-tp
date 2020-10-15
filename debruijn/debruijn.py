d18d9b925de0d0726354097a4e70870f1e3d97e4
MAEL <MAEL@MacBook-Air-de-MAEL.local>
2020-10-15 15:15:59 +0200

    Ajout du calcul de contigs

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -18,6 +18,7 @@ import os
 import sys
 import networkx as nx
 import matplotlib
+import matplotlib.pyplot as plt
 from operator import itemgetter
 import random
 random.seed(9001)
@@ -66,6 +67,8 @@ def get_arguments():
 
 
 def read_fastq(fastq):
+    '''Renvoie les séquences contenues dans un fichier fastq
+    '''
     with open(fastq, "r") as filin:
         for line in filin:
             yield(next(filin).strip())
@@ -74,11 +77,15 @@ def read_fastq(fastq):
 
 
 def cut_kmer(seq, k):
+    '''Renvoie les k-mer de taille k d'une séquence
+    '''
     for base in range(len(seq)-k+1):
         yield(seq[base:base+k])
 
 
 def build_kmer_dict(fastq, k):
+    '''Renvoie dictionnaire de k-mer
+    '''
     dict_kmer = {}
     for seq in read_fastq(fastq):
         for kmer in cut_kmer(seq, k):
@@ -86,11 +93,75 @@ def build_kmer_dict(fastq, k):
                 dict_kmer[kmer] = 1
             else:
                 dict_kmer[kmer] += 1
+
     return(dict_kmer)
 
 
 def build_graph(dict_kmer):
-    pass
+    '''Créer graphe à partir des k-mers
+    '''
+    G = nx.DiGraph()
+    for key, value in dict_kmer.items():
+        a = key[:-1]
+        b = key[1:]
+        G.add_node(a)
+        G.add_node(b)
+        G.add_edge(a, b, weight = value)
+    #nx.draw(G, with_labels = True)
+    #plt.show()
+    return(G)
+
+
+def get_starting_nodes(G):
+    '''Trouver nœuds d'entrée
+    '''
+    list_start = []
+    for node in G.nodes.data():
+        if not list(g.predecessors(node[0])):
+            list_start.append(node[0])
+
+    return(list_start)
+
+
+def get_sink_nodes(G):
+    '''Trouver nœuds de sortie
+    '''
+    list_end = []
+    for node in G.nodes.data():
+        if not list(g.successors(node[0])):
+            list_end.append(node[0])
+
+    return(list_end)
+
+
+def get_contigs(g, list_start, list_end):
+    '''Retourner tuple de contigs
+    '''
+    list_contig = []
+    for start in list_start:
+        for end in list_end:
+            for path in nx.all_simple_paths(g, start, end):
+                list_contig.append((path, len(path)))
+
+    return(list_contig)
+
+
+def fill(text, width=80):
+    """Split text with a line return to respect fasta format"""
+    return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
+
+
+def save_contigs(list_tupple, filename):
+    '''Sauvegarder contigs dans fichier de sortie
+    '''
+    with open(filename, "w") as filout:
+        for i in range(len(list_tupple)):
+            contig = "".join(list_tupple[i][0][0])
+            for read in range(1, len(list_tupple[i][0])):
+                contig = contig + list_tupple[i][0][read][-1]
+
+            filout.write(">contig_{} len={}\n{}\n".format(i, len(contig), fill(contig)))
+
 
 #==============================================================
 # Main program
@@ -104,8 +175,12 @@ def main():
 
 if __name__ == '__main__':
 
-    for i in cut_kmer("ATTTACCGGAGCGATT", 7):
-        print(i)
+    g = build_graph(build_kmer_dict("../data/eva71_two_reads.fq", 20))
+    start = get_starting_nodes(g)
+    end = get_sink_nodes(g)
+    contig = get_contigs(g, start, end)
+    save_contigs(contig, "output.fasta")
+
     main()
 
 
